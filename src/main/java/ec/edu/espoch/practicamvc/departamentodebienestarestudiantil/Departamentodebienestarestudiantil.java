a0061aa95fe8e5b175605cbec93c5bd35fcc5685
erikalizbethhhhhhhhh@gmail.com <tuemail@ejemplo.com>
2024-11-26 11:11:04 -0500

    erika

--- src/main/java/ec/edu/espoch/practicamvc/departamentodebienestarestudiantil/Departamentodebienestarestudiantil.java
+++ src/main/java/ec/edu/espoch/practicamvc/departamentodebienestarestudiantil/Departamentodebienestarestudiantil.java
@@ -11,6 +11,6 @@ package ec.edu.espoch.practicamvc.departamentodebienestarestudiantil;
 public class Departamentodebienestarestudiantil {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Bienvenido sea");
     }
 }
